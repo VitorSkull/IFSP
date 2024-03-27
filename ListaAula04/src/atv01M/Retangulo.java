86f7d3f9a2dca62a02126347a47682dbfe45e7de
VitorSkull <138405618+VitorSkull@users.noreply.github.com>
2024-03-27 19:21:39 -0300

    Atividades da Aula04

---
+++ ListaAula04/src/atv01M/Retangulo.java
@@ -0,0 +1,10 @@
+package atv01M;
+
+public class Retangulo extends Poligono {
+	
+	@Override
+	public void area() {
+		
+	}
+
+}
---
+++ ListaAula04/src/atv01M/main.java
@@ -0,0 +1,10 @@
+package atv01M;
+
+public class main {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
